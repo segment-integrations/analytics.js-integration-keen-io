6aabd404974d7a424daec19e84eb6835f6cf365f
Devin Ekins <devinj.ekins@gmail.com>
2015-06-26 11:30:50 -0600

    Global Properties now refresh on track()
    Previously if a client were to call identify() to set some user state,
    load a different page, and then call track(), the user state would be
    lost as Keen does not persist the GlobalProperties across page loads.
    
    Now, much of the identify() work is performed within track() if user
    state is present.  This updates the GlobalProperties before sending the
    event to Keen, saving the developer from having to call identify() before
    every track() call, just in case.
    
    References issue #3 on Github.

--- test/index.test.js
+++ test/index.test.js
@@ -259,6 +259,21 @@ describe('Keen IO', function() {
         });
       });
 
+      it('should always refresh global properties with user traits if present', function() {
+        var id = 'Devon Higgins';
+        var traits = { trait: 'Pretty Neat', id: id };
+        analytics.user().userId = id;
+        analytics.user().traits(traits);
+
+        // Notice no identify() call is made here.
+        // This is to emulate a page reload which loses the global
+        // properties that identify() saves.
+
+        analytics.track('event');
+        var global_user = keen.client.client.globalProperties().user;
+        analytics.deepEqual(global_user, { userId: id, traits: traits });
+      });
+
       describe('addons', function() {
         it('should add ipAddon if enabled', function() {
           keen.options.ipAddon = true;
