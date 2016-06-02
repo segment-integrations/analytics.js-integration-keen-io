7abb1fa1a74410e8226fa71b40ba76eb8cd0a835
Nathan Houle <nathan@nathanhoule.com>
2016-06-02 13:09:33 -0700

    Automatically migrate as many dependencies as possible

--- test/index.test.js
+++ test/index.test.js
@@ -1,8 +1,8 @@
 
-var Analytics = require('analytics.js-core').constructor;
-var integration = require('analytics.js-integration');
-var sandbox = require('clear-env');
-var tester = require('analytics.js-integration-tester');
+var Analytics = require('@segment/analytics.js-core').constructor;
+var integration = require('@segment/analytics.js-integration');
+var sandbox = require('@segment/clear-env');
+var tester = require('@segment/analytics.js-integration-tester');
 var Keen = require('../lib/');
 
 describe('Keen IO', function() {
